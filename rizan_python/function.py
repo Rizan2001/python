449cfe2883d96347dfbaeb901a72e5d297cdabf5
Rizan2001 <rizan.mr23@gmail.com>
2024-11-02 00:37:28 -0700

    Add files via upload

---
+++ rizan_python/function.py
@@ -0,0 +1,35 @@
+# def function(fname):
+#     print("rizz" + fname)
+# function(" khan")
+
+# def function(fname):
+#     print("rizz" + fname)
+# function(" khan")
+
+# def function(fname):
+#     print("anarkali" + fname)
+# function(" marikar")
+
+
+# def function(fname):
+#     print(fname + "george")
+# function("joju ")
+# function("nivin ")
+# function("vineeth ")
+
+# def addition (x,y):
+#     sum = x + y
+#     print ("ans", sum)
+# addition (2,3)
+
+# def subtraction (x,y):
+#     sum = x - y
+#     print ("ans", sum)
+# subtraction(4,5)
+
+def multiplication (u,r):
+    sum = u * r
+    print ("ans", sum)
+multiplication(5,3)
+
+
---
+++ rizan_python/oper.py
@@ -0,0 +1,62 @@
+# x = 5
+# x += 3
+# print(x)
+
+# x = 2
+# y = 3
+# print(x + y)
+
+# a = 5
+# a += 3
+# print(a)
+
+# c = 2
+# c -= 2
+# print(c)
+
+# d = 1
+# d *= 2
+# print(d)
+
+# e = 2
+# e /= 5
+# print(e)
+
+# f = 3
+# f %= 5
+# print(f)
+
+# g = 5
+# g //= 3
+# print(g)
+
+# h = 5
+# h **= 3
+# print(h)
+
+a = 5
+b = 3
+print(a == b)
+
+c = 5
+d = 3
+print(c != d)
+
+e = 5
+f = 3
+print(e > f)
+
+e = 5
+f = 3
+print(e < f)
+
+g = 5 
+h = 3
+print(g >= h)
+
+i = 5
+j = 3
+print(i <= j)
+
+a = 5
+print(a >5 and a<10)
